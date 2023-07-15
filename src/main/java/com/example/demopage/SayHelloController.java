7bbb741cc826cfaa36b2837acdf00c5f78b867a5
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-07-15 18:09:51 +0200

    Add delay to allow the user to see the spinner

--- src/main/java/com/example/demopage/SayHelloController.java
+++ src/main/java/com/example/demopage/SayHelloController.java
@@ -11,14 +11,17 @@ import static java.util.Objects.isNull;
 @RestController
 public class SayHelloController {
 
+    public static final int LEAVE_TIME_FOR_SEEING_THE_SPINNERS = 600;
+
     @PostMapping(value = "/api/v1/hello",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> sayHello(@RequestBody HelloRequest request) {
+    public ResponseEntity<?> sayHello(@RequestBody HelloRequest request) throws InterruptedException {
         if (isNull(request.getName())) {
             return ResponseEntity.badRequest().body(new ErrorResponse("Missing parameter 'name'"));
         }
         String message = String.format("Hello, %s!", request.getName());
+        Thread.sleep(LEAVE_TIME_FOR_SEEING_THE_SPINNERS);
         return ResponseEntity.ok(new HelloResponse(message));
     }
 }
