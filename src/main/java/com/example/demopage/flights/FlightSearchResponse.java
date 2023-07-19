96733610ba5decbda308e0be002fbf23fbd4f8fd
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-07-19 08:00:44 +0200

    Make one result the clear "min" in the price results table

--- src/main/java/com/example/demopage/flights/FlightSearchResponse.java
+++ src/main/java/com/example/demopage/flights/FlightSearchResponse.java
@@ -16,7 +16,7 @@ public record FlightSearchResponse(String[][] prices) {
                         departureDate.minusDays(1).toString(), "€99", "€103", "€299", "€201", "€112",
                 },
                 {
-                        departureDate.toString(), "€101", "€102", "€199", "€202", "€101",
+                        departureDate.toString(), "€101", "€102", "€199", "95", "€101",
                 },
                 {
                         departureDate.plusDays(1).toString(), "€100", "€103", "€299", "€201", "€102",
@@ -24,7 +24,6 @@ public record FlightSearchResponse(String[][] prices) {
                 {
                         departureDate.plusDays(2).toString(), "€99", "€103", "€299", "€201", "€112",
                 },
-
         };
 
 
